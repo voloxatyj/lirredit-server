a0ffa68a01a659267efbf3fdff29d334227ed9d9
Medynskyy Ostap <medinsky_os@groupbwt.com>
2023-05-26 13:56:44 +0300

    add templates to sendEmail feature

--- src/utils/sendEmai.ts
+++ src/utils/sendEmai.ts
@@ -3,7 +3,7 @@ import handlebars from 'handlebars';
 import fs from 'fs';
 import path from 'path';
 
-export const sendEmail = async (email: string, subject: string, payload: object, template) => {
+export const sendEmail = async (email: string, subject: string, payload: object, template: string) => {
   const testAccount = await nodemailer.createTestAccount();
 
   const transporter = nodemailer.createTransport({
