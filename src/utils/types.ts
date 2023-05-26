d065a2c9b9c2fd16c80706c3dc607e69e83ab1e7
Medynskyy Ostap <medinsky_os@groupbwt.com>
2023-05-26 13:58:49 +0300

    declare interface to get userId from session

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -2,9 +2,15 @@ import { Field, ObjectType } from '@nestjs/graphql';
 import { Request, Response } from 'express';
 import { Session, SessionData } from 'express-session';
 
+declare module 'express-session' {
+  interface SessionData {
+    userId: number;
+  }
+}
+
 export type MyContext = {
   req: Request & {
-    session: Session & Partial<SessionData> & { UserID: number };
+    session: Session & Partial<SessionData>;
   };
   res: Response;
 };
